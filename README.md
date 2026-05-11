c3bbd1586edf434f6995aa081a8b097dac177f1f
Mariusz Jurgielewicz <mordor@mail.com>
2026-05-10 19:42:51 -0700

    Getting Started

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rust-rpico2-embassy-examples
-rust-rpico2-embassy-examples Rust Embassy examples for Raspberry Pi Pico 2
+# Rust Embassy examples for Raspberry Pi Pico 2
+
+Rust Embassy examples for Raspberry Pi Pico 2 (RP2350)
+
+## Usage
+
+Project generated:
+
+```sh
+cargo generate --git https://github.com/ImplFerris/pico2-template.git
+```
+
+## Options
+
+- HAL: Choose between Embassy (async) or rp-hal
+- defmt logging: Optional debugging support
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embassy_rp as hal;
+use embassy_executor::Spawner;
+use embassy_rp::block::ImageDef;
+use embassy_time::Timer;
+
+//Panic Handler
+use {panic_probe as _};
+// Defmt Logging
+use defmt_rtt as _;
+
+/// Tell the Boot ROM about our application
+#[unsafe(link_section = ".start_block")]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Examples for RP2350 (Embassy)");
+    let _p = embassy_rp::init(Default::default());
+    
+    loop{
+        Timer::after_millis(100).await;
+    }   
+}
+
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recommended to have these minimal entries.
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"rust-rpico2-embassy-examples"),
+    embassy_rp::binary_info::rp_program_description!(
+        c"your program description"
+    ),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+// End of file
